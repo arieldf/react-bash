bfd68edc8fe1f625590300198de5fcd3df2c1125
Dimitri Racordon <dimitri.racordon@gmail.com>
2017-03-01 23:18:54 +0100

    Adds tests for promised extensions.

--- tests/bash.js
+++ tests/bash.js
@@ -73,6 +73,30 @@ describe('bash class methods', () => {
             chai.assert.isFunction(bash.execute);
         });
 
+        it('should handle fulfilled promises', () => {
+            const bash = new Bash({
+                test: { exec: (state) => Promise.resolve('foo') }
+            });
+
+            const testCase = bash.execute('test', {});
+            return testCase.then((newState) => {
+                chai.assert.strictEqual(newState, 'foo');
+            });
+        });
+
+        it('should handle rejected promises', () => {
+            const bash = new Bash({
+                test: { exec: (state) => Promise.reject(new Error('foo')) }
+            });
+
+            const testCase = bash.execute('test', mockState);
+            return testCase.then((newState) => {
+                const { history } = newState;
+                chai.assert.strictEqual(history.length, 1);
+                chai.assert.strictEqual(history[0].value, 'foo');
+            });
+        });
+
         // Full command testing is in tests/command.js
         const commands = [
             { command: 'help' },
